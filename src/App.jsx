d8ab25d77b9e3fecfdb5e6c55f6fc2c77a432df4
Philip Traas <philip.traas@airbus.com>
2024-12-17 15:20:16 +0100

    added player component and logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import './App.css'
+import './index.css'
 import { Header } from './Components/Containers/Header/Header';
 import { Footer } from './Components/Containers/Footer/Footer';
 import { Button } from './Components/UI/Button/Button';
 import { User } from './Components/Containers/User/User';
+import { Player } from './Components/Containers/Player/Player';
 
 function App(){
 
@@ -11,6 +12,7 @@ function App(){
       <Header />
       <User />
       <Footer />
+      <Player />
       <Button />
     </div>
   );
---
+++ src/Components/Containers/Player/Player.jsx
@@ -0,0 +1,13 @@
+export function Player() {
+    const score = 300;
+    let result = score >= 500 ? "winner" : "loser";
+
+
+
+
+    return(
+        <>
+        {result}
+        </>
+    )
+}
