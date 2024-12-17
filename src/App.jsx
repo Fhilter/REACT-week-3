3bfc92ec9c1d88539b13956528a1ca15d953aeb4
Philip Traas <philip.traas@airbus.com>
2024-12-17 11:04:59 +0100

    removed h1 from footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Header } from './Components/Containers/Header/Header';
 import { Footer } from './Components/Containers/Footer/Footer';
