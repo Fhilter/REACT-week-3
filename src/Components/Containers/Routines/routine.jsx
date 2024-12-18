7b7ef5e8e7c6341edff05b0dd1b16f496854fb96
Philip Traas <philip.traas@airbus.com>
2024-12-18 12:34:22 +0100

    added functions

---
+++ src/Components/Containers/Routines/routine.jsx
@@ -0,0 +1,34 @@
+import { Button } from "../../UI/Button/Button";
+
+export function Routine() {
+
+
+
+    
+
+        function clickRoutine(action) {
+            alert(`Time to ${action}!`)
+        } 
+
+        return(
+
+            <>
+            <h2>My Routines</h2>
+            <Button
+            callBack={ function() {clickRoutine('EAT')}}>
+            EAT
+            </Button>
+            <Button
+            callBack={function() {clickRoutine('SLEEP')}}>
+            SLEEP
+            </Button>
+            <Button
+            callBack={function() {clickRoutine('RAVE')}}>
+            RAVE
+            </Button>
+            <h2>REPEAT</h2>
+            </>
+
+        )
+
+}
