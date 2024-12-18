ce7a5d8ba54fa2667ae0ed1fe8874b5d1b5df1c3
Philip Traas <philip.traas@airbus.com>
2024-12-18 16:33:45 +0100

    added hobby input

--- src/Components/Containers/User/Hobbies.jsx
+++ src/Components/Containers/User/Hobbies.jsx
@@ -1,11 +1,33 @@
-export function Hobbies({ hobbies }) {
+import { useState } from "react"
+import { Button } from "../../UI/Button/Button"
 
-    // const hobbies = [{name: "Cycling", id: "uib25FHPQEF"}, {name: "Lego", id: "iafboafbw23"}, {name: "F1", id: "uqh98yqe2w9"}]
+export function Hobbies({ hobbies = [] }) {
+
+    const [newHobby, setNewHobby] = useState('')
+    const [inputError, setInputError] = useState("")
+    const [initialHobbies, setInitialHobbies] = useState(hobbies)
+
+
+    function getHobbyInput(event) {
+        const value = event.target.value
+        setNewHobby(value)
+    }
+
+    function submitHobby() {
+        if (newHobby === "") {
+            setInputError('You forgot to add your hobby')
+            return
+        }
+        setInitialHobbies([...initialHobbies, { id: initialHobbies.length + 1, name: newHobby.trim() }])
+        alert('Congrats, you now like ' + newHobby)
+            setNewHobby("")
+            setInputError("")  
+    }
 
     return(
         <div>
             <p>My Hobbies are:</p>
-            {hobbies.map((hobby) => {
+            {initialHobbies.map((hobby) => {
                 return(
                     <li key={hobby.id} style={{
                         padding: '5px',
@@ -13,6 +35,14 @@ export function Hobbies({ hobbies }) {
                     }}>{hobby.name}</li>
                 )
             })}
+            <input 
+            placeholder="Add a hobby"
+            onChange={getHobbyInput}
+            className="bg-neutral-800 p-2 w-full border-2"
+            type="text"
+            />
+            <p className="text-red-600 text-sm">{inputError}</p>
+            <Button callBack={submitHobby}>Add Hobby</Button>
         </div>
     )
 }
